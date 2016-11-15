a76ba715779b81ce1a726b600b06c91be2355c26
Duke Nukeme <nkm@semitransparentdesign.com>
2016-11-15 19:49:59 +0900

    modal beautify

--- 009.tetsuya-umeda/modal.js
+++ 009.tetsuya-umeda/modal.js
@@ -1,41 +1,33 @@
-//modal
-$(function(){
- 
-  //テキストリンクをクリックしたら
-    $("#modal-open").click(function(){
-      //body内の最後に<div id="modal-bg"></div>を挿入
-        $("body").append('<div id="modal-bg"></div>');
- 
-    //画面中央を計算する関数を実行
-    modalResize();
- 
-    //モーダルウィンドウを表示
-        $("#modal-bg,#modal-main").fadeIn("slow");
- 
-    //画面のどこかをクリックしたらモーダルを閉じる
-        $("#modal-bg,#modal-main").click(function(){
-            $("#modal-main,#modal-bg").fadeOut("slow",function(){
-          //挿入した<div id="modal-bg"></div>を削除
-                $('#modal-bg').remove() ;
-            });
- 
-        });
- 
-    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
-        $(window).resize(modalResize);
-        function modalResize(){
- 
-            var w = $(window).width();
-            var h = $(window).height();
- 
-            var cw = $("#modal-main").outerWidth();
-            var ch = $("#modal-main").outerHeight();
- 
-        //取得した値をcssに追加する
-            $("#modal-main").css({
-                "left": ((w - cw)/2) + "px",
-                "top": ((h - ch)/2) + "px"
-            });
-        }
+$(function() {
+    var vimeoPlayer = $("<iframe>");
+
+    vimeoPlayer.attr("src", "https://player.vimeo.com/video/181209409")
+               .attr("frameborder", "0")
+               .attr("webkitallowfullscreen", true)
+               .attr("mozallowfullscreen", true)
+               .attr("allowfullscreen", true)
+               .attr("id", "vimeo-player");
+
+    function resizeModal() {
+        var playerWidth = $(window).width() * 0.8;
+        var playerHeight = (playerWidth / 640) * 360;
+        vimeoPlayer.attr("width", (playerWidth + "px"))
+                   .attr("height", (playerHeight + "px"));
+    }
+
+    function closeModal(ev) {
+        ev.preventDefault();
+        $("body").removeClass("modal-open");
+        $("#vimeo-player").remove();
+    }
+
+    $("#modal-link").on("click", function(ev) {
+        ev.preventDefault();
+        resizeModal();
+        $("#modal").append(vimeoPlayer);
+        $("body").addClass("modal-open");
     });
+
+    $("#modal").on("click", closeModal);
+    $(window).on("resize", resizeModal);
 });
